19d7f95a686c5d7e669757d2694fd7bfeff7d2be
gunjan <singhgunjan129312@gmail.com>
2026-08-03 22:20:36 +0530

    PlacementGernany

---
+++ track/M01-understanding-java/DemoProgram.java
@@ -0,0 +1,10 @@
+public class DemoProgram{
+    public static void main(String[] args) {
+        System.out.println("String java");
+        System.out.println("char to integers");
+        System.out.println("---MY PLACEMENT GOAL ---");
+        System.out.println("Role Software Developer");
+        System.out.println("Target: Get Placed");
+        System.err.println("Daily Practice: 8 Hours");
+    }
+}
