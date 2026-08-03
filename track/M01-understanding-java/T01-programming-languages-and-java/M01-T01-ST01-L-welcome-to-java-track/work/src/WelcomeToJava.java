4bdb619f39f46af9269b1bab28978b469af091a0
gunjan <singhgunjan129312@gmail.com>
2026-08-03 21:32:32 +0530

    AddLine

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,6 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
        System.out.println("Welcome to java track");
+       System.out.println("I am ready to begin");
     }
 }
