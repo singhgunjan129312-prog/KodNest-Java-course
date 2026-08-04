0cb1af490e81bf07425de910cb1049594ac787fb
gunjan <singhgunjan129312@gmail.com>
2026-08-04 14:54:11 +0530

    DisplayGitRep

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/DisplayGitRep.java
@@ -0,0 +1,10 @@
+public class DisplayGitRep {
+    public static void main(String[] args) {
+
+        // Display the repository cloning status.
+        System.out.println("Repository: Cloned Successfully");
+
+        // Display whether the project is ready.
+        System.out.println("Project Status: Ready for Development");
+    }
+}
