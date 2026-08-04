ab8079b22be53668dafdf004c6e60cfd97cbf349
gunjan <singhgunjan129312@gmail.com>
2026-08-05 00:34:27 +0530

    StuPro

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/studentProfile.java
@@ -0,0 +1,12 @@
+public class studentProfile{
+    public static void main(String[] args){
+                // Prints a simple student ID card
+        System.out.println("--------------------");
+        System.out.println("STUDENT PROFILE");
+        System.out.println("Name: Alex");
+        System.out.println("Course: Java");
+        System.out.println("Level: Beginner");
+        System.out.println("--------------------");
+
+    }
+}
