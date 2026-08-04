efbb58db101d8af2040c24008227f133eae73e79
gunjan <singhgunjan129312@gmail.com>
2026-08-04 11:32:18 +0530

    Day

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/practice.java
@@ -0,0 +1,11 @@
+public class practice{
+        public static void main(String[] args){
+            System.err.println("Monday");
+            System.err.print("Tuesday");
+            System.err.print("Wednesday");
+            System.out.println("Thusrsday");
+            System.out.print("Friday");
+            System.out.println("Saturday");
+            System.out.print("Sunday");
+        }
+}
