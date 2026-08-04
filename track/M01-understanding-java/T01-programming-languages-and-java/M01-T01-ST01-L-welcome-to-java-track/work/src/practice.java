f01d6794dc50891f3b03db1733242a8b27b3c33b
gunjan <singhgunjan129312@gmail.com>
2026-08-04 12:27:38 +0530

    Positive

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/practice.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/practice.java
@@ -2,7 +2,7 @@ public class practice{
         public static void main(String[] args){
             System.err.println("Monday");
             System.err.print("Tuesday");
-            System.err.print("Wednesday");
+            System.err.println("Wednesday");
             System.out.println("Thusrsday");
             System.out.print("Friday");
             System.out.println("Saturday");
