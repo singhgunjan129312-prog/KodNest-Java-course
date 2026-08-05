aa48fe90f5b8a98dd406820eb24d31aab9074873
gunjan <singhgunjan129312@gmail.com>
2026-08-05 12:31:54 +0530

    whileLoop

--- track/M01-understanding-java/M02/ForLoop1.java
+++ track/M01-understanding-java/M02/ForLoop1.java
@@ -1,7 +1,14 @@
 public class ForLoop1{
     public static void main(String[] args){
+        //for loop
         for(int i=1;i<=5;i++){
             System.out.println("i");
+        }
+        //while loop
+        int i=1;
+        while(i<=5){
+            System.out.println("i");
+            i++;
         }
     }
 }
