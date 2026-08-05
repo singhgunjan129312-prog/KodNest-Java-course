b9b72fcaace62e6dddfe51fa705f8963c0b245c0
gunjan <singhgunjan129312@gmail.com>
2026-08-05 15:19:38 +0530

    switch1

---
+++ track/M01-understanding-java/M02/switch1.java
@@ -0,0 +1,36 @@
+public class switch1 {
+    public static void main(String[] args) {
+
+        int roundCode = 4;
+        int attemptsUsed = 2;
+        int maxAttempts = 3;
+        String roundName;
+
+        // Switch statement
+        switch (roundCode) {
+            case 1:
+                roundName = "Aptitude Round";
+                break;
+            case 2:
+                roundName = "Technical Round";
+                break;
+            case 3:
+                roundName = "HR Round";
+                break;
+            case 4:
+            case 5:
+                roundName = "Final Review";
+                break;
+            default:
+                roundName = "Invalid Round";
+        }
+
+        // Ternary expression
+        String attemptStatus = (attemptsUsed < maxAttempts)
+                ? "Attempt Available"
+                : "Attempt Limit Reached";
+
+        System.out.println("Current Round: " + roundName);
+        System.out.println("Attempt Status: " + attemptStatus);
+    }
+}
