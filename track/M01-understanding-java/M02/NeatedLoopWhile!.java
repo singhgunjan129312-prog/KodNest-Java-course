73c95a70ed715a5318d7d72ddae59671600b8ba5
gunjan <singhgunjan129312@gmail.com>
2026-08-06 11:44:35 +0530

    NeLooWhile

--- track/M01-understanding-java/M02/NeatedLoopWhile!.java
+++ track/M01-understanding-java/M02/NeatedLoopWhile!.java
@@ -1,7 +1,9 @@
-public class NestedLoopFor1{
+public class NeatedLoopWhile1{
     public static void main(String[] args){
-        for(int i=1;i<=5;i++){
-            for(int j=1;j<=5;j++){
+        int i=1;
+        while(i<=5){
+            int j=1;
+            while(j<=5){
                 System.out.println(j);
             }
             System.err.println("\n");
