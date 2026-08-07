6a9d6fd0631781786b954d80acdd3538696e0ac8
gunjan <singhgunjan129312@gmail.com>
2026-08-07 12:05:12 +0530

    UserInput3

---
+++ track/M01-understanding-java/M02/UserTypeInput3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class UserTypeInput3{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter char: ");
+        char ch=sc.next().charAt(2);
+        System.out.println(ch);
+
+    }
+}
