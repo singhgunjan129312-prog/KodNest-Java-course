d3471a3edaa1c6e64c2e95fecddcd6c6df190ed4
gunjan <singhgunjan129312@gmail.com>
2026-08-11 00:23:13 +0530

    PlacementEligibilityCheck

---
+++ track/M01-understanding-java/M02/PlacementEligibilityCheck.java
@@ -0,0 +1,14 @@
+public class PlacementEligibilityCheck{
+    public static void main(String[] args) {
+        int marks = 72;
+        int attendance = 80;
+        boolean projectCompleted = true;
+
+        boolean marksEligible = marks >= 60;
+        boolean attendanceEligible = attendance >= 75;
+        boolean academicEligible = marksEligible && attendanceEligible;
+        boolean placementEligible = academicEligible && projectCompleted;
+
+        System.out.println(placementEligible);
+    }
+}
