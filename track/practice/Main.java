c785b4368bf066d34700145c88c2f54e121a1b11
gunjan <singhgunjan129312@gmail.com>
2026-08-12 15:31:59 +0530

    FoodOrderDelivery

---
+++ track/practice/Main.java
@@ -0,0 +1,15 @@
+class A{
+    void m(String a,String b){
+        System.out.println("Hello");
+    }
+    void m(A a,A b){
+        System.out.println("Hi");
+    }
+}
+public class Main{
+    public static void main(String[] args) {
+        A a1 = new A();
+        
+       
+    }
+}
