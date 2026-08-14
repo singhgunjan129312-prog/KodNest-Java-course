cf51fa34fb24be9f780ca2eb226b3ac040d9f7a2
gunjan <singhgunjan129312@gmail.com>
2026-08-14 14:43:04 +0530

    CreateMobilePlanConstructerOverLOad

---
+++ track/M01-understanding-java/M02/CreateMobilePlanOverLoadCons.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+class MobilePlan {
+
+    String planName;
+    int price;
+
+    // Default constructor
+    MobilePlan() {
+        this.planName = "Basic";
+        this.price = 199;
+    }
+
+    // Parameterized constructor
+    MobilePlan(String planName, int price) {
+        this.planName = planName;
+        this.price = price;
+    }
+
+    // Display method
+    void display() {
+        System.out.println("Plan: " + planName + ", Price: " + price);
+    }
+}
+
+public class CreateMobilePlanOverLoadCons {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String planName = scanner.nextLine();
+        int price = scanner.nextInt();
+
+        // Default constructor
+        MobilePlan defaultPlan = new MobilePlan();
+
+        // Parameterized constructor
+        MobilePlan selectedPlan = new MobilePlan(planName, price);
+
+        defaultPlan.display();
+        selectedPlan.display();
+
+        scanner.close();
+    }
+}
