9552524e3f6b6ca8f7991847b93bea6fdd9f89c0
gunjan <singhgunjan129312@gmail.com>
2026-08-14 14:19:08 +0530

    RobotApplication

---
+++ track/M01-understanding-java/M02/RobotApp.java
@@ -0,0 +1,15 @@
+class Robot {
+
+    Robot() {
+        System.out.println("Beep beep! Robot reporting for Java duty!");
+    }
+}
+
+public class RobotApp {
+
+    public static void main(String[] args) {
+
+        Robot robot = new Robot();
+
+    }
+}
