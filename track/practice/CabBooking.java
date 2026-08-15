b13e3c19ce692ae0b0527b200bd13625cf7f1490
gunjan <singhgunjan129312@gmail.com>
2026-08-15 17:31:18 +0530

    CabBookingProcess

---
+++ track/practice/CabBooking.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+class CabBooking {
+
+    String passengerName;
+    String cabType;
+    int distance;
+    int fare;
+
+    // Quick booking constructor
+    public CabBooking(String passengerName) {
+        this.passengerName = passengerName;
+        this.cabType = "Mini";
+        this.distance = 5;
+        this.fare = 75;
+    }
+
+    // Custom booking constructor
+    public CabBooking(String passengerName, String cabType, int distance) {
+        this.passengerName = passengerName;
+        this.cabType = cabType;
+        this.distance = distance;
+        this.fare = distance * 15;
+    }
+
+    // Display booking
+    void displayBooking() {
+        System.out.println("Passenger: " + passengerName);
+        System.out.println("Cab Type: " + cabType);
+        System.out.println("Distance: " + distance + " km");
+        System.out.println("Fare: Rs. " + fare);
+    }
+}
+
+public class CabBooking {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int choice = sc.nextInt();
+        String passengerName = sc.next();
+
+        CabBooking c;
+
+        if (choice == 1) {
+            c = new CabBooking(passengerName);
+        } 
+        else if (choice == 2) {
+            String cabType = sc.next();
+            int distance = sc.nextInt();
+
+            c = new CabBooking(passengerName, cabType, distance);
+        } 
+        else {
+            return;
+        }
+
+        c.displayBooking();
+
+        sc.close();
+    }
+}
---
+++ track/practice/quation/MobileUnlockPro.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+
+
+
+class UnlockMobile{
+    void findFingerPrintSenser(FingerPrint  f){
+        System.out.println("FingerPrintSenser Found");
+    }
+    void findFaceRec(Face f1){
+        System.out.println("Face Recognition");
+    }
+    void findPattern(Pattern p){
+        System.out.println("Pattern recgonised");
+    }
+    void findPin(int Pin ){
+        System.out.println("pin recgonised");
+    }
+    void findVoice(Voice v){
+        System.out.println("Voice recgonised");
+    }
+
+}
+class FingerPrint{
+
+}
+class Face{
+
+}
+class Pattern{
+
+}
+class Pin{
+
+}
+class Voice{
+
+}
+public class MobileUnlockPro{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        UnlockMobile u=new UnlockMobile();
+        
+
+    }
+}
