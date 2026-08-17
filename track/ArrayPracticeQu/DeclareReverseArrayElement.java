92bda1abb87af19727c75670ca62fc1eadb98d93
gunjan <singhgunjan129312@gmail.com>
2026-08-17 12:52:34 +0530

    ReverseArrayDeclareElement

---
+++ track/ArrayPracticeQu/DeclareReverseArrayElement.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class DeclareReverseArrayElement{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[] a=new int[5];
+        System.out.println("Enter the Array Element:");
+        for(int i=a.length-1;i>=0;i--){
+            //for(i=0;i<a.length;i++)
+            a[i]=sc.nextInt();}
+        }
+        System.out.println("Enter the values of Array:");
+        for (int i=a.length-1;i>=0;i--){
+            // for(i=0;i<a.length;i++)
+            System.out.println(a[i]);
+        }
+   }
+}
+    
