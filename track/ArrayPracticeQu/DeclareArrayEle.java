a7aea6296bc70e6a5150656fd4cca8a122b6d64d
gunjan <singhgunjan129312@gmail.com>
2026-08-17 11:57:39 +0530

    FirstArrayDeclareElemengt

---
+++ track/ArrayPracticeQu/DeclareArrayEle.java
@@ -0,0 +1,18 @@
+public  class DeclareArrayEle{
+    public static void main(String[] args) {
+        // Declare the arrayName
+        int []arr=new int[5];
+        // Assign the value of variable
+        arr[0]=10;
+        arr[1]=20;
+        arr[3]=30;
+        arr[4]=40;
+        arr[2]=50;
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+
+    }
+}
