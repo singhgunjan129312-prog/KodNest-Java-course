0ce87a584c5795387366890064ab41ca8312c119
gunjan <singhgunjan129312@gmail.com>
2026-08-17 12:01:30 +0530

    SecondDEclareArrayElementByLetrals

---
+++ track/ArrayPracticeQu/DeclareArrayEle1.java
@@ -0,0 +1,13 @@
+public  class DeclareArrayEle1{
+    public static void main(String[] args) {
+        // Declare the arrayName
+        int []arr={10,20,30,40,50};
+        // Assign the value of variable
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+
+    }
+}
