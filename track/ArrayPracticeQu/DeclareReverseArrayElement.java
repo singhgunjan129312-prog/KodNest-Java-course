6eb07d7d225dd774401b7987c62b1e84550cbe16
gunjan <singhgunjan129312@gmail.com>
2026-08-17 20:04:15 +0530

    TraceSencerReading

--- track/ArrayPracticeQu/DeclareReverseArrayElement.java
+++ track/ArrayPracticeQu/DeclareReverseArrayElement.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class DeclareReverseArrayElement{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int[] a=new int[5];
+        int a[]=new int[5];
         System.out.println("Enter the Array Element:");
         for(int i=a.length-1;i>=0;i--){
             //for(i=0;i<a.length;i++)
