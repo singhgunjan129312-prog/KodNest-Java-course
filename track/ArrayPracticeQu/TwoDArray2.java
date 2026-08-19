2f53d33571edba714265c5ff90876158aad19fd9
gunjan <singhgunjan129312@gmail.com>
2026-08-19 12:43:09 +0530

    TwoDArrayUsingLengthPro

--- track/ArrayPracticeQu/TwoDArray2.java
+++ track/ArrayPracticeQu/TwoDArray2.java
@@ -4,11 +4,11 @@ public class TwoDArray2{
         Scanner sc=new Scanner(System.in);
         int[][] arr=new int[3][5];
         System.out.println("Enter the Array Element: ");
-        for (int i=0;i<3;i++){
-            for(int j=0;j<5;j++){
+        for (int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
                 arr[i][j]=sc.nextInt();
             }
-            System.out.println();
+            
         }
         System.out.println("Enter the array value are: ");
          for (int i=0;i<arr.length;i++){
