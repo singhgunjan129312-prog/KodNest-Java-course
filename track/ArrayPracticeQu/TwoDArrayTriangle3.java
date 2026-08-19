21e233cfcad4abd9af1c2dbbc5f9f239fa9bdf88
gunjan <singhgunjan129312@gmail.com>
2026-08-20 00:07:10 +0530

    PrintTriangleMatrix

--- track/ArrayPracticeQu/TwoDArrayTriangle3.java
+++ track/ArrayPracticeQu/TwoDArrayTriangle3.java
@@ -1,24 +1,24 @@
+
 import java.util.Scanner;
+
 public class TwoDArrayTriangle3{
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int[][] arr=new int[3][];
-        int[] arr[0] = new int[4];
-        int[] arr[1] =new int[3];
-        int 
-        System.out.println("Enter the Array Element: ");
-        for (int i=0;i<3;i++){
-            for(int j=0;j<5;j++){
-                arr[i][j]=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int arr[][]=new int[3][];
+         arr[0]=new int[4];
+         arr[1]=new int[3];
+         arr[2]=new int[5];
+         for (int i = 0;i < arr.length;i++) {
+            for (int j = 0;j < arr[i].length;j++) {
+               arr[i][j] = sc.nextInt();
             }
-            System.out.println();
-        }
-        System.out.println("Enter the array value are: ");
-         for (int i=0;i<3;i++){
-            for(int j=0;j<5;j++){
-                System.out.print(arr[i][j] + " ");
+         }
+         for (int i = 0;i < arr.length;i++) {
+            for (int j = 0;j < arr[i].length;j++) {
+              System.out.print(arr[i][j] + " ");
             }
             System.out.println();
          }
+
     }
 }
