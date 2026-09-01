3b831f3067d8703278278066dbe2a75626b81aa0
gunjan <singhgunjan129312@gmail.com>
2026-09-01 15:58:43 +0530

    CheckWhetherArrayElementIsSort

--- track/MOD_3/CheckWhetherArrayIsSort.java
+++ track/MOD_3/CheckWhetherArrayIsSort.java
@@ -1,19 +1,15 @@
 import java.util.Scanner;
 
-public class Main {
+public class CheckWhetherArrayIsSort {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        // ऐरे का साइज रीड करना
-        int n = sc.nextInt();
+                int n = sc.nextInt();
         int[] a = new int[n];
         
-        // ऐरे के एलिमेंट्स रीड करना
-        for (int i = 0; i < n; i++) {
+         for (int i = 0; i < n; i++) {
             a[i] = sc.nextInt();
         }
         
-        // चेक करना कि ऐरे सॉर्टेड है या नहीं
         int sort = 1;
         for (int i = 0; i < n - 1; i++) {
             if (a[i] > a[i + 1]) {
@@ -22,8 +18,7 @@ public class Main {
             }
         }
         
-        // रिजल्ट प्रिंट करना (1 मतलब सॉर्टेड है, 0 मतलब नहीं है)
-        System.out.println(sort);
+           System.out.println(sort);
         
         sc.close();
     }
