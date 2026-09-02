324d6196568184be4cbb00236e4e20de6168bf0a
gunjan <singhgunjan129312@gmail.com>
2026-09-02 15:13:56 +0530

    MoveZeroElementInsideLastPositionInArray

---
+++ track/MOD_3/MoveAllZeroArrEleInsideLast.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class MoveAllZeroArrEleInsideLast {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+       
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        
+
+        int writeIdx = 0;
+        for (int readIdx = 0; readIdx < n; readIdx++) {
+            if (a[readIdx] != 0) {
+                a[writeIdx] = a[readIdx];
+                writeIdx++;
+            }
+        }
+        
+        while (writeIdx < n) {
+            a[writeIdx] = 0;
+            writeIdx++;
+        }
+        
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
+        }
+        
+        sc.close();
+    }
+}
