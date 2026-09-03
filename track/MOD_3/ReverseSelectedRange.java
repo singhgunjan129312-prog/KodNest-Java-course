6bc04de72fd882aaa9fc7a582d5c2944e8741378
gunjan <singhgunjan129312@gmail.com>
2026-09-03 15:19:40 +0530

    REverseArrayOfSelectedRange

--- track/MOD_3/ReverseSelectedRange.java
+++ track/MOD_3/ReverseSelectedRange.java
@@ -1,7 +1,7 @@
-import java.util.Scannner;
+import java.util.Scanner;
 public class ReverseSelectedRange {
-    public ststic void main(String[] args) {
-        Scannner sc = new Scanner(System.in);
+    public static  void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] a = new int[n];
         for (int i = 0;i < n;i++) {
