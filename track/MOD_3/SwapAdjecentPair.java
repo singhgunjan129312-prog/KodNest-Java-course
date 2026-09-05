91b298de856d825dbce76191367e4758713eebd6
gunjan <singhgunjan129312@gmail.com>
2026-09-05 16:00:56 +0530

    SwapAdjecentPairValue

---
+++ track/MOD_3/SwapAdjecentPair.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class SwapAdjecentPair{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<n-1;i += 2){
+            int temp=a[i];
+            a[i]=a[i+1];
+            a[i+1]=temp;
+            
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(a[i] + " ");
+        }
+    }
+}
