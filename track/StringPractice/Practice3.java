7a305d8ce0e8f61974d5f691bcb80a1013fefdcf
gunjan <singhgunjan129312@gmail.com>
2026-09-07 17:28:07 +0530

    PracticeStringQuation3

---
+++ track/StringPractice/Practice3.java
@@ -0,0 +1,12 @@
+public class Practice3{
+    public static void main(String[] args){
+        String s1 = "java";
+        String s2 = "java";
+        if(s1.equals(s2)){
+            System.out.println("String are same");
+        } else {
+            System.out.println("String are not same");
+        }
+    }
+
+}
