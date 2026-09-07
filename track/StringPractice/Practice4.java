18fdc933729a71fd010362957e3f885d12b67f84
gunjan <singhgunjan129312@gmail.com>
2026-09-07 17:30:38 +0530

    PracticeStringQuation4

---
+++ track/StringPractice/Practice4.java
@@ -0,0 +1,12 @@
+public class Practice4{
+    public static void main(String[] args){
+        String s1 = new String("java");
+        String s2 = new String("java");
+        if(s1.equals(s2)){
+            System.out.println("String are same");
+        } else {
+            System.out.println("String are not same");
+        }
+    }
+
+}
