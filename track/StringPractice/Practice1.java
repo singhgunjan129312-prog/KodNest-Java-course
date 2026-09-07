b02216357d037050d35775f5087977ec26c5f098
gunjan <singhgunjan129312@gmail.com>
2026-09-07 17:22:19 +0530

    PracticeStringQuation1

---
+++ track/StringPractice/Practice1.java
@@ -0,0 +1,12 @@
+public class Practice1{
+    public static void main(String[] args){
+        String s1 = "java";
+        String s2 = "java";
+        if(s1==s2){
+            System.out.println("Ref are equal");
+        } else {
+            System.out.println("Ref are not  equal");;
+        }
+    }
+
+}
