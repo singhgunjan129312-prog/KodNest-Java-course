e6a9971b6ed2b94778b5c219006e2e76730bbf42
gunjan <singhgunjan129312@gmail.com>
2026-09-13 22:49:31 +0530

    ConvertStringBuilderToString

--- track/Mod4/ConvertBuiToString.java
+++ track/Mod4/ConvertBuiToString.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class ConvertBuiToString {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
