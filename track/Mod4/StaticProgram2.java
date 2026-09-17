c79a8d5b4660ebb6bbf7e9299e4c2e3b4a0b2c49
gunjan <singhgunjan129312@gmail.com>
2026-09-17 17:26:40 +0530

    StaticBlockExeOnly1timeAndnonSBlockExeNoofObjAfterObjCre

---
+++ track/Mod4/StaticProgram2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Demo{
+    static {
+        System.out.println("1st static-block executed.");
+    }
+    static {
+        System.out.println("2nd static-block executed.");
+    }
+    static {
+        System.out.println("3rd static-block executed.");
+    }
+    {
+        System.out.println("1st non-static-block executed.");
+    }
+    {
+        System.out.println("2nd non-static-block executed.");
+    }
+    {
+        System.out.println("3rd non-static-block executed.");
+    }
+}
+public class StaticProgram2{
+    public static void main(String[] args) {
+        Demo d1 = new Deew Demo();
+        Demo d2 = new Demo();
+        Demo d3 = new Demo();
+    }
+}
