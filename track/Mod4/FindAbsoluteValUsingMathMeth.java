ff45b23df7c326881914c5133ea811d9661cce1c
gunjan <singhgunjan129312@gmail.com>
2026-09-19 16:19:21 +0530

    FindAbsoluteValueUsingMathMethod

---
+++ track/Mod4/FindAbsoluteValUsingMathMeth.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class FindAbsoluteValUsingMathMeth{
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int abs = Math.abs(a);
+    System.out.println("Absolute value: " + abs);
+}
