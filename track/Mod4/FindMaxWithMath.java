37eea209e2577965134cf89d94cf9b5f688847bc
gunjan <singhgunjan129312@gmail.com>
2026-09-19 16:17:15 +0530

    FindMaximumValueUsingMathMethod

---
+++ track/Mod4/FindMaxWithMath.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class FindMaxWithMath{
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int max = Math.max(a,b);
+    System.out.println("Maximum: " + max);
+}
